17d71e25ab4bb1aaa2a0445336a3ec77f2a43ea3
Brian Douglass <corp@tps-eoltp.com>
2020-04-20 07:56:54 -0700

    --Added all the original scaffold items

---
+++ ExceptionTest/Areas/Identity/Data/ExceptionTestContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExceptionTest.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExceptionTest.Data
+{
+    public class ExceptionTestContext : IdentityDbContext<ExceptionTestUser>
+    {
+        public ExceptionTestContext(DbContextOptions<ExceptionTestContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
